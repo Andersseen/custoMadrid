ca7ae1e3b554971f6fe3bc4ff7f41fc1c02452c4
Andrey <forestx24@gmail.com>
2021-09-14 15:46:36 +0200

    added animation on scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,38 @@ window.onscroll = () => {
 }
 
 
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+
+/*SCROLL HOME*/
+sr.reveal('.header__title', { origin: 'bottom' })
+sr.reveal('.header__social', { origin: 'bottom', delay: 200 })
+sr.reveal('.header__subtitle', { origin: 'bottom', delay: 400 })
+
+/*SCROLL BLOG*/
+sr.reveal('.blog__img', { origin: 'right' })
+sr.reveal('.blog__wrapper', { origin: 'left' })
+
+/*SCROLL GALLERY*/
+sr.reveal('.box__1', { origin: 'right' })
+sr.reveal('.box__2', {})
+sr.reveal('.box__3', { origin: 'left' })
+sr.reveal('.box__4', { origin: 'right' })
+sr.reveal('.box__5', { origin: 'bottom' })
+sr.reveal('.box__6', { origin: 'left' })
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img', { origin: 'left' })
+sr.reveal('.about__wrapper', { origin: 'right' })
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__title', {})
+
+
+
